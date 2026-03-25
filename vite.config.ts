59a5cf4a4f74207d7fb89430b11d7b7b5291403b
vijumolo <vijumolo@github.local>
2026-03-25 14:21:31 -0500

    Configurar GitHub Actions para publicar en GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/NuevaAgendaTCT/',
   plugins: [
     react(),
     tailwindcss(),
